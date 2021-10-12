df4480cf3fcb96fa1d0c0f05f0d804c360dbd422
Dazzler Kumar <dazzler.deepak98@gmail.com>
2021-10-12 15:08:55 +0530

    Animations Added

--- components/Main.js
+++ components/Main.js
@@ -1,14 +1,19 @@
 import Image from "next/image"
-
+import Fade from "react-reveal/Fade"
+import Bounce from "react-reveal/Bounce"
 
 export default function Main() {
     return (
         <div className="MainPage place-items-center">
-            <div className=" mx-auto hover:rotate-180">
-                <Image src="/main-img.png" width={300} height={300}/>
+            <Bounce top delay={1000} duration={1500}>
+            <div className=" mx-auto">
+                <Image src="/main-img.png" width={300} height={300} quality={75}/>
             </div>
+            </Bounce>
             <div className="mx-auto">
+                <Fade right cascade duration={2000}>
             <h1 className="text-main text-5xl font-bold mx-2 sm:text-center">The Great Indian Traffic</h1>
+                </Fade>
             <h3 className="subtitle text-2xl font- md:text-center mx-2">Indian Traffic Jams are all about filling the gaps.</h3>
             </div>
            
