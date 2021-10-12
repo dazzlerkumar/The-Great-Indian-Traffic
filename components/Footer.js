9a7c35ab36263d2666b45e554d950688b71a1099
yash jain <yashsj24@gmail.comgit config --global user.name yash>
2021-10-12 19:36:07 +0530

    Change text color of footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,11 @@
 
 export default function Footer() {
     return (
-        <div className="grid py-10 cool-gradient max-w-full place-items-center mt-20">
+        <div className="grid py-10 cool-gradient max-w-full place-items-center mt-20 text-neela">
            <div className="mx-auto">
-            <p className="text-4xl font-bold text-center">thank you!</p>   
+            <p className="text-4xl font-bold text-center">thank you!</p>
             <p className="text-2xl font-semilbold text-center">do share if your enjoyed reading!</p>
-            </div>           
+            </div>
         </div>
     )
 }
