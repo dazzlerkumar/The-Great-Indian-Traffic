b5f88eade3237289225e07f827da015bc56e4e7d
Neha Koli <nehakoli78@gmail.com>
2021-10-14 22:49:53 +0530

    Changed the font of title and footer

--- components/Footer.js
+++ components/Footer.js
@@ -3,8 +3,8 @@ export default function Footer() {
     return (
         <div className="grid py-10 cool-gradient max-w-full place-items-center mt-20">
            <div className="mx-auto">
-            <p className="text-4xl font-bold text-center">thank you!</p>   
-            <p className="text-2xl font-semilbold text-center">do share if your enjoyed reading!</p>
+            <p className="text-4xl font-bold text-center font-link">Thank You!</p>   
+            <p className="text-2xl font-semilbold text-center">Do share if your enjoyed reading!</p>
             </div>           
         </div>
     )
